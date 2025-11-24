61f8e3e8662bb800ef5e85ad852bf6ba02734f95
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2025-11-24 21:16:22 +0200

    refactor: use constant for "open" modifier, change modifier names, remove excess code from css

--- scripts/script.js
+++ scripts/script.js
@@ -5,18 +5,19 @@ const wrap = document.querySelector(".wrapper");
 const mobile = document.querySelector(".mobile");
 const btnMenu = document.querySelector(".btn__head__menu");
 const navs = document.querySelector(".navs");
+const openMod = "open";
 
 btnMenu.addEventListener("click", function () {
-  if (!btnMenu.classList.contains("rotate--open")) {
-    btnMenu.classList.add("rotate--open");
-    navs.classList.add("open--head");
-    mobile.classList.add("mob--menu");
+  if (!btnMenu.classList.contains(openMod)) {
+    btnMenu.classList.add(openMod);
+    navs.classList.add(openMod);
+    mobile.classList.add(openMod);
     document.body.style.overflowY = "hidden";
     wrap.style.paddingTop = "unset";
-  } else if (btnMenu.classList.contains("rotate--open")) {
-    btnMenu.classList.remove("rotate--open");
-    navs.classList.remove("open--head");
-    mobile.classList.remove("mob--menu");
+  } else if (btnMenu.classList.contains(openMod)) {
+    btnMenu.classList.remove(openMod);
+    navs.classList.remove(openMod);
+    mobile.classList.remove(openMod);
     document.body.removeAttribute("style");
     wrap.removeAttribute("style");
   }
@@ -37,7 +38,7 @@ function ppWindow(btnOpen, pp) {
   function closeWindow() {
     pp.classList.remove("active");
 
-    if (!navs.classList.contains("open--head")) {
+    if (!navs.classList.contains(openMod)) {
       document.body.removeAttribute("style");
     }
   }
