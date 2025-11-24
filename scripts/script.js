447a97dce63dc5243559196ec56e6b6ad87306ba
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2025-11-25 00:13:39 +0200

    refactor: remove unnecessary selectors (.mobile, .navigation)

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,6 @@ typeof WOW !== "undefined" && new WOW().init();
 typeof AOS !== "undefined" && AOS.init();
 
 const wrap = document.querySelector(".wrapper");
-const mobile = document.querySelector(".mobile");
 const btnMenu = document.querySelector(".btn__head__menu");
 const navs = document.querySelector(".navs");
 const openMod = "open";
@@ -11,13 +10,11 @@ btnMenu.addEventListener("click", function () {
   if (!btnMenu.classList.contains(openMod)) {
     btnMenu.classList.add(openMod);
     navs.classList.add(openMod);
-    mobile.classList.add(openMod);
     document.body.style.overflowY = "hidden";
     wrap.style.paddingTop = "unset";
   } else if (btnMenu.classList.contains(openMod)) {
     btnMenu.classList.remove(openMod);
     navs.classList.remove(openMod);
-    mobile.classList.remove(openMod);
     document.body.removeAttribute("style");
     wrap.removeAttribute("style");
   }
