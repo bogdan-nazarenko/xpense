4a0336c1fca9d242ae32b309d8806102b94b82a2
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2026-05-01 20:13:31 +0300

    refactor: restructure the project

--- scripts/script.js
+++ scripts/script.js
@@ -1,43 +1,47 @@
-typeof AOS !== "undefined" && AOS.init();
-typeof WOW !== "undefined" && new WOW().init();
+new WOW({ offset: 500 }).init();
+AOS.init({ offset: 500, delay: 200, duration: 500, once: true });
 
 const ACTIVE = "active";
 const OPEN = "open";
+const NON_SCROLLABLE = "non-scrollable";
+const PADDING_TOP_RESET = "padding-top--reset";
 
 const wrapper = document.querySelector(".wrapper");
-const headerNav = document.querySelector(".header__nav");
-const headerMenuButton = document.querySelector(".header__menu__button");
+const headerMenu = document.querySelector(".header__menu");
+const headerMenuButton = document.querySelector(".header__menu-button");
 
 headerMenuButton.addEventListener("click", function () {
-  const isOpen = headerNav.classList.contains(OPEN);
-
-  document.body.style.overflowY = !isOpen
-    ? "hidden"
-    : document.body.removeAttribute("style");
-  wrapper.style.paddingTop = !isOpen
-    ? "initial"
-    : wrapper.removeAttribute("style");
-  headerNav.classList.toggle(OPEN);
-  headerMenuButton.classList.toggle(ACTIVE);
+  document.body.classList.toggle(NON_SCROLLABLE);
+  wrapper.classList.toggle(PADDING_TOP_RESET);
+  headerMenu.classList.toggle(OPEN);
+  this.classList.toggle(ACTIVE);
 });
 
-const headerNavButton = document.querySelector(".header__nav__button");
-const demoButton = document.querySelector(".demo__button");
-const popupDemo = document.querySelector(".popup_demo");
-const popupContainer = document.querySelector(".popup__container");
-const popupCloseButton = document.querySelector(".popup__close__button");
+const mqlMinWidth768 = window.matchMedia("(width >= 768px)");
 
-headerNavButton.addEventListener("click", () => popupDemo.show());
-demoButton.addEventListener("click", () => popupDemo.show());
-popupDemo.addEventListener("click", () => popupDemo.close());
-popupContainer.addEventListener("click", (event) => event.stopPropagation());
-popupCloseButton.addEventListener("click", () => popupDemo.close());
-
-window.addEventListener("resize", function () {
-  if (window.innerWidth >= 768 && headerNav.classList.contains(OPEN)) {
-    document.body.removeAttribute("style");
-    wrapper.removeAttribute("style");
-    headerNav.classList.remove(OPEN);
-    headerMenuButton.classList.remove(OPEN);
+mqlMinWidth768.addEventListener("change", function () {
+  if (this.matches) {
+    document.body.classList.remove(NON_SCROLLABLE);
+    wrapper.classList.remove(PADDING_TOP_RESET);
+    headerMenu.classList.remove(OPEN);
+    headerMenuButton.classList.remove(ACTIVE);
   }
 });
+
+const forms = document.querySelectorAll(".support__form, .footer__form");
+
+forms.forEach((form) => {
+  form.addEventListener("submit", (event) => event.preventDefault());
+});
+
+const headerButton = document.querySelector(".header__button");
+const heroButton = document.querySelector(".hero__button");
+const modal = document.querySelector(".modal");
+const modalCloseButton = document.querySelector(".modal__close-button");
+
+headerButton.addEventListener("click", () => modal.showModal());
+heroButton.addEventListener("click", () => modal.showModal());
+modal.addEventListener("click", function (event) {
+  if (event.target === this) this.close();
+});
+modalCloseButton.addEventListener("click", () => modal.close());
