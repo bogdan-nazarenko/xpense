ab5e72676d196033519821f8530d86ea9dc56430
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2026-09-04 20:23:10 +0300

    refactor: restructure and update the project

---
+++ scripts/sections/support.js
@@ -0,0 +1,7 @@
+const supportForm = document.querySelector(".support__form");
+
+function supportFormHandler(event) {
+  event.preventDefault();
+}
+
+supportForm.addEventListener("submit", supportFormHandler);
